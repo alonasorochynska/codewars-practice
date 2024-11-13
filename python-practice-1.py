4bafe70c3a7c9fa658b9a0c0f1e81d704aa06ff8
Alona <alona.sorochynska@gmail.com>
2024-11-13 23:30:52 +0000

    solution

--- python-practice-1.py
+++ python-practice-1.py
@@ -300,4 +300,18 @@ def persistence(n):
     return mult
 
 
-print(persistence(999), "\n")
+# Task 20
+"""
+Take 2 strings s1 and s2 including only letters from a to z. Return a new
+sorted string (alphabetical ascending), the longest possible, containing
+distinct letters - each taken only once - coming from s1 or s2.
+"""
+
+
+def longest(a1, a2):
+    # return "".join(sorted(set(s1).union(s2)))
+    # return "".join(sorted(set(a1 | a2)))
+    return "".join(sorted(set(a1 + a2)))
+
+
+print(longest("aretheyhere", "yestheyarehere"), "\n")
